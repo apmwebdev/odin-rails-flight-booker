d1ca12865a9fbfa022b190da72b8f843d321aa59
Austin Miller <austinmillerwebdev@gmail.com>
2023-01-06 08:43:28 -0500

    Set up Airport - Flight associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,4 @@
 class Flight < ApplicationRecord
-  belongs_to :departure_airport
-  belongs_to :destination_airport
+  belongs_to :departure_airport, class_name: "Airport"
+  belongs_to :destination_airport, class_name: "Airport"
 end
